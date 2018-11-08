a6e0ad8472aceb1f01ec2ed6258f0dc4d3844215
TheRealVira <therealvira@gmail.com>
2018-11-08 11:19:04 +0100

    added base project and files

---
+++ story-teller/Learning/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sources: https://www.azlyrics.com/m/mychemicalromance.html");
+
+
+
+            Console.ReadKey(false);
+        }
+    }
+}
